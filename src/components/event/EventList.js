3c1c2b3744e31690a71b779ba9bb74be42c2d6c8
Jasmin Kaset <kasetski@gmail.com>
2021-02-15 07:17:56 -0600

    join and leave button toggles allowing user to join or leave events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,16 +1,71 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { EventContext } from "./EventProvider.js"
 import { useHistory } from "react-router-dom"
 
 export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
+    const [toggle, setToggle]=useState(false)
     const history = useHistory()
     useEffect(() => {
         getEvents()
     }, [])
 
     return (
+        <>
         <article className="events">
+            <header className="events__header">
+                <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
+            </header>
+            {
+                events.map(event => {
+                    const attending = events.some(evt => evt.id === event.id)
+                    return <section key={event.id} className="registration">
+                        
+                        <div className="registration__game">GAME NIGHT: {event.game.title}</div>
+
+                        <div>{event.description}</div>
+                        <div>Where? {event.location}</div>
+
+
+                        <div>When? {
+                            new Date().toLocaleDateString("en-US",
+                                {
+                                    weekday: 'long',
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric'
+                                })
+
+                        }
+                        </div>
+
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => {
+                                        // console.log("leave",event)
+                                        leaveEvent(event.id).then(setToggle)}}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => {
+                                        // console.log("join",event)
+                                        joinEvent(event.id).then(setToggle)}}
+                                    >Join</button>
+                        }
+                    </section>
+                })
+            }
+        </article >
+        </>
+    )
+}
+
+{/* <article className="events">
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
             </header>
@@ -18,7 +73,7 @@ export const EventList = (props) => {
                 events.map(event => {
                     return <section key={event.id} className="registration">
                         <div className="registration__game">GAME NIGHT: {event.game.title}</div>
-                        {/* <div>{event.description}</div> */}
+                        <div>{event.description}</div> 
                         <div>Where? {event.location}</div>
                         
 
@@ -42,6 +97,4 @@ export const EventList = (props) => {
             history.push({ pathname: "/events/new" })
         }}
         >Register New Event</button>
-        </article >
-    )
-}
+        </article > */}
--- src/components/event/EventProvider.js
+++ src/components/event/EventProvider.js
@@ -3,11 +3,11 @@ import React, { useState } from "react"
 export const EventContext = React.createContext()
 
 export const EventProvider = (props) => {
-    const [ events, setEvents ] = useState([])
+    const [events, setEvents] = useState([])
 
     const getEvents = () => {
         return fetch("http://localhost:8000/events", {
-            headers:{
+            headers: {
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`
             }
         })
@@ -16,21 +16,45 @@ export const EventProvider = (props) => {
     }
 
     const createEvent = (event) => {
-        return fetch("http://localhost:8000/events", { 
+        return fetch("http://localhost:8000/events", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Token ${localStorage.getItem("lu_token")}`
-            }, 
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            },
             body: JSON.stringify(event)
-          })
-          
+        })
+
             .then(getEvents)
+
+    }
+
+
+
+    const leaveEvent = eventId => {
+        return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+            method: "DELETE",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
             
-      } 
+            .then(getEvents)
+    }
+    
+    const joinEvent = eventId => {
+        return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+            method: "POST",
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(getEvents)
+    }
     return (
-        <EventContext.Provider value={{ events, getEvents, createEvent }} >
-            { props.children }
+        <EventContext.Provider value={{ events, getEvents, createEvent, leaveEvent, joinEvent }} >
+            { props.children}
         </EventContext.Provider>
     )
 }
